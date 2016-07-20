05748523411ad0adaa1d6a745dd5ad1d7cc6af69
caramel-koala <antoniob.peters@gmail.com>
2016-07-20 19:04:13 +0200

    finished 4.4.3 [2]

--- 4.4.3/q2.m
+++ 4.4.3/q2.m
@@ -5,11 +5,11 @@ clc
 %a
 
 %vary Swedish points
-x(1,:)=linspace(24,46,10);
+x(1,:)=linspace(24,46,100);
 %vary school quality
-x(2,:)= 1:10;
+x(2,:)= linspace(1,10,100);
 %fix test results
-x(3,:)=50*ones(10,1);
+x(3,:)=50*ones(100,1);
 %deploy the net
 y=ugradnet(x);
 %plots
@@ -30,11 +30,11 @@ title('semester 2 vs Swedish points for a fixed test result of 50')
 %b
 
 %Fix Swedish points
-xb(1,:)=30*ones(10,1);
+xb(1,:)=30*ones(100,1);
 %vary school quality
-xb(2,:)= 1:10;
+xb(2,:)= linspace(1,10,100);
 %vary test results
-xb(3,:)=linspace(26,46,10);
+xb(3,:)=1:100;
 %deploy the net
 yb=ugradnet(xb);
 %plots
@@ -50,3 +50,22 @@ title('semester 1 vs Test results for a fixed Swedish points of 30')
 figure
 plot(xb(3,:),yb(2,:))
 title('semester 2 vs Test results for a fixed Swedish points of 30')
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%c
+
+%Fix Swedish points
+xc(1,:)=25*ones(100,1);
+%vary school quality
+xc(2,:)= 6*ones(100,1);
+%vary test results
+xc(3,:)=1:100;
+%deploy the net
+yb=ugradnet(xb);
+%plots
+figure
+plot(xb(3,:),yb(1,:))
+title('semester 1 vs Test results for a fixed Swedish points of 25 and School Quality of 6')
+figure
+plot(xb(3,:),yb(2,:))
+title('semester 2 vs Test results for a fixed Swedish points of 25 and School Quality of 6')
